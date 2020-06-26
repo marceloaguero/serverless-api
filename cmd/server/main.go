5b5895a687ddc558fc06b16691aebb45bdc68a43
Marcelo Aguero <marceloaguero@gmail.com>
2020-06-26 17:41:21 -0300

    Complete server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
+
+	transport "github.com/marceloaguero/serverless-api/users/transports/http"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 
+	router, err := transport.Routes()
+	if err != nil {
+		log.Panic(err)
+	}
+
 	log.Println("Running on port: ", port)
+	log.Panic(http.ListenAndServe(":"+port, router))
 }
