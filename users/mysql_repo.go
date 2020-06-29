c3d07ae7ddca5d9cf42449f727ff5d9b74637887
Marcelo Aguero <marceloaguero@gmail.com>
2020-06-29 19:41:42 -0300

    Complete MySQL CRUD

--- users/mysql_repo.go
+++ users/mysql_repo.go
@@ -3,6 +3,7 @@ package users
 import (
 	"context"
 	"database/sql"
+	"log"
 	"os"
 	"time"
 
@@ -49,7 +50,8 @@ func mysqlConnect(dsName, dbName string) (*sql.DB, error) {
 
 // Create a user
 func (r *MysqlDBRepo) Create(ctx context.Context, user *User) error {
-	insertQuery := "INSERT INTO " + r.tableName + "(id, email, name, age) VALUES (?, ?, ?, ?)"
+	insertQuery := "INSERT INTO " + r.tableName +
+		"(id, email, name, age) VALUES (?, ?, ?, ?)"
 	stmt, err := r.db.PrepareContext(ctx, insertQuery)
 	if err != nil {
 		return err
@@ -61,20 +63,75 @@ func (r *MysqlDBRepo) Create(ctx context.Context, user *User) error {
 
 // Get a user
 func (r *MysqlDBRepo) Get(ctx context.Context, id string) (*User, error) {
-	return nil, nil
+	user := &User{}
+	selectQuery := "SELECT id, email, name, age FROM " +
+		r.tableName +
+		" WHERE id = ?"
+
+	err := r.db.QueryRowContext(ctx, selectQuery, id).Scan(
+		&user.ID, &user.Email, &user.Name, &user.Age,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
 
 // GetAll users
 func (r *MysqlDBRepo) GetAll(ctx context.Context) ([]*User, error) {
-	return nil, nil
+	users := make([]*User, 0)
+	selectQuery := "SELECT id, email, name, age FROM " +
+		r.tableName
+
+	rows, err := r.db.QueryContext(ctx, selectQuery)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		user := &User{}
+		if err := rows.Scan(
+			&user.ID, &user.Email, &user.Name, &user.Age,
+		); err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return users, nil
 }
 
 // Update a user
 func (r *MysqlDBRepo) Update(ctx context.Context, id string, user *UpdateUser) error {
-	return nil
+	updateQuery := "UPDATE " +
+		r.tableName +
+		" SET email = ?, name = ?, age = ? WHERE id = ?"
+	stmt, err := r.db.PrepareContext(ctx, updateQuery)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.ExecContext(ctx, user.Email, user.Name, user.Age, id)
+	return err
 }
 
 // Delete a user
 func (r *MysqlDBRepo) Delete(ctx context.Context, id string) error {
-	return nil
+	deleteQuery := "DELETE FROM " +
+		r.tableName +
+		" WHERE id = ?"
+	stmt, err := r.db.PrepareContext(ctx, deleteQuery)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.ExecContext(ctx, id)
+	return err
 }
