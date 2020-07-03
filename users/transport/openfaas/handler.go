0056cfc29c9a997677db1a3884c74f53d419da6a
Marcelo Aguero <marceloaguero@gmail.com>
2020-07-03 17:24:20 -0300

    Add DB connection in function

--- users/transport/openfaas/handler.go
+++ users/transport/openfaas/handler.go
@@ -3,18 +3,27 @@ package function
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/marceloaguero/serverless-api/users"
+	mysqlrepo "github.com/marceloaguero/serverless-api/users/repository/mysql"
 )
 
 func init() {
-	fmt.Println("Como dice que le va?")
 	port := os.Getenv("PORT")
 	dsName := os.Getenv("DB_DSN")
 	dbName := os.Getenv("DB_NAME")
 	tableName := os.Getenv("TABLE_NAME")
 
-	fmt.Println(port, dsName, dbName, tableName)
+	repository, err := mysqlrepo.NewMysqlRepo(dsName, dbName, tableName)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	usecase := users.NewUsecase(repository)
+	fmt.Println(usecase)
 }
 
 // Handle is the input point in openfaas
