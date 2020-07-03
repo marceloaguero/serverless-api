67ccb08fb66aa9ac272a65ce5991b75653724eee
Marcelo Aguero <marceloaguero@gmail.com>
2020-07-03 15:41:39 -0300

    Add init func

--- users/transport/openfaas/handler.go
+++ users/transport/openfaas/handler.go
@@ -4,8 +4,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
+func init() {
+	fmt.Println("Como dice que le va?")
+	port := os.Getenv("PORT")
+	dsName := os.Getenv("DB_DSN")
+	dbName := os.Getenv("DB_NAME")
+	tableName := os.Getenv("TABLE_NAME")
+
+	fmt.Println(port, dsName, dbName, tableName)
+}
+
+// Handle is the input point in openfaas
 func Handle(w http.ResponseWriter, r *http.Request) {
 	var input []byte
 
